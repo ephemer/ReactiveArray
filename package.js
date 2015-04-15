e4e29e48e4c71eec288718fde158cd984e0e2f13
Geordie J <geojay@gmail.com>
2015-04-15 14:58:22 +0200

    Update package metadata

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
-  summary: "Use Blaze's ReactiveVar to make a self-contained reactive array (with push, shift etc.)",
+  summary: "Use ReactiveVar to make a self-contained reactive array (with push, shift etc.)",
   version: "1.0.0",
-  git: ""
+  git: "https://github.com/ephemer/ReactiveArray"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.1.1');
   api.use(['reactive-var', 'underscore']);
-  api.addFiles('ephemer:reactive-array.js', 'client');
+  api.addFiles('reactive-array.js', 'client');
   api.export('ReactiveArray');
 });
 
